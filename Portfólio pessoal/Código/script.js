596ee7f8f2e8a718dd7f1da6379c5892d349d471
Luan Galli <luan.galli@sempreceub.com>
2025-05-10 15:08:57 -0300

    Add files via upload

---
+++ Portfólio pessoal/Código/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Navegação manual
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// Mostrar slides
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 5000); // muda a cada 5 segundos
